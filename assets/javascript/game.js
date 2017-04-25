679d67ecfa69025ed2084af78937b508046acd5d
Cullan Shewfelt <cullanshewfelt@github.com>
2017-04-25 14:28:36 -0700

    initial

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+var config = {
+            apiKey: "AIzaSyC2zX21Sr9rRePNQuySb7C-tqo0LWmakgE",
+            authDomain: "rpsmmorpg.firebaseapp.com",
+            databaseURL: "https://rpsmmorpg.firebaseio.com",
+            projectId: "rpsmmorpg",
+            storageBucket: "rpsmmorpg.appspot.com",
+            messagingSenderId: "706557831253"
+        };
+        firebase.initializeApp(config);
+
+	var userPresent = false;
+	var playerOne;
+	var playerTwo;
+
+
+
+
+
+
+
+
+})
